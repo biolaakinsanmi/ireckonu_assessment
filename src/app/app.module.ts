7cc922be4be28ddf80d4092750804b7a47bfc87b
Biola Akinsanmi <biola.akinsanmi@gmail.com>
2019-08-26 12:12:18 +0100

    Added major page components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,16 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ProfileListComponent } from './profile-list/profile-list.component';
+import { Page404Component } from './page404/page404.component';
+import { ViewProfileComponent } from './view-profile/view-profile.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProfileListComponent,
+    Page404Component,
+    ViewProfileComponent
   ],
   imports: [
     BrowserModule,
