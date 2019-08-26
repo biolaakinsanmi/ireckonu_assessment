2ea49d36276c38620527c8a3c830663da31304cd
Biola Akinsanmi <biola.akinsanmi@gmail.com>
2019-08-26 12:13:14 +0100

    Set up main routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,28 @@
+import { ProfileListComponent } from './profile-list/profile-list.component';
+import { Page404Component } from './page404/page404.component';
+import { ViewProfileComponent } from './view-profile/view-profile.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '/profiles',
+    pathMatch: 'full'
+  },
+  {
+    path:  'profiles',
+    component:  ProfileListComponent
+  },
+  {
+    path:  'profile/:id',
+    component:  ViewProfileComponent
+  },
+  {
+    path: '**',
+    component: Page404Component
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
